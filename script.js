d21fd335f50231b3ccb0316d2fe97d7668674f55
Ankush Aich <jayaaich48@gmail.com>
2025-12-13 12:18:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let age = 28;
+if(age > 18){
+    console.log("Can watch porn")
+}
+else{
+    console.log("Cannot watch porn")
+}
---
+++ vote.js
@@ -0,0 +1,16 @@
+const a = 1.1;
+
+if(a > 18){
+    console.log("Can drive")
+}
+
+else if (a < 18 && a > 1){
+    console.log("Can't drive")
+}
+else if(a == 0){
+    console.log("Hey you new born baby stop using the computer!")
+}
+
+else{
+    console.log("Invalid")
+}
